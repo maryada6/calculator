afe827bccfde748ae01e1d90f5296555ad091e35
maryada <jainsubhamd@gmail.com>
2022-07-20 10:36:48 +0530

    Optimise validator

--- pkg/validator/validator_test.go
+++ pkg/validator/validator_test.go
@@ -16,3 +16,20 @@ func TestStringInList(t *testing.T) {
 		assert.Equal(t, false, stringInList("random", commandList))
 	})
 }
+
+func TestValidator(t *testing.T) {
+	t.Run("should return false is tokens does not have 2 tokens", func(t *testing.T) {
+		tokens := []string{"add", "5", "up"}
+		assert.Equal(t, false, Validator(tokens))
+	})
+
+	t.Run("should return true for valid tokens", func(t *testing.T) {
+		tokens := []string{"add", "5"}
+		assert.Equal(t, true, Validator(tokens))
+	})
+
+	t.Run("should return false for invalid tokens", func(t *testing.T) {
+		tokens := []string{"added", "5"}
+		assert.Equal(t, false, Validator(tokens))
+	})
+}
