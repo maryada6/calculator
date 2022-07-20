3bc1cc4947fca75017d2640dd751097dd1145a72
maryada <jainsubhamd@gmail.com>
2022-07-20 10:31:47 +0530

    Introduce word in a list

--- pkg/validator/validator.go
+++ pkg/validator/validator.go
@@ -1,1 +1,23 @@
 package validator
+
+import "strconv"
+
+var commandList = []string{"add", "subtract", "multiply", "divide", "cancel", "exit"}
+
+func stringInList(operation string, list []string) bool {
+	for _, item := range list {
+		if item == operation {
+			return true
+		}
+	}
+	return false
+}
+
+func Validator(tokens []string) bool {
+	if len(tokens) != 2 {
+		return false
+	}
+	operation, value := tokens[0], tokens[1]
+	_, err := strconv.ParseFloat(value, 64)
+	return stringInList(operation, commandList) && err == nil
+}
--- pkg/validator/validator_test.go
+++ pkg/validator/validator_test.go
@@ -1,1 +1,18 @@
 package validator
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestStringInList(t *testing.T) {
+	t.Run("should return true for a string in our given list", func(t *testing.T) {
+		var commandList = []string{"add", "subtract", "multiply", "divide", "cancel", "exit"}
+		assert.Equal(t, true, stringInList("add", commandList))
+	})
+
+	t.Run("should return false for a string not in our given list", func(t *testing.T) {
+		var commandList = []string{"add", "subtract", "multiply", "divide", "cancel", "exit"}
+		assert.Equal(t, false, stringInList("random", commandList))
+	})
+}
